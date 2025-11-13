a755ee1da11aab76fa69acdea470d3c88a033d57
Randy <randyrobinson3227@gmail.com>
2025-11-13 11:29:00 +0300

    iou

--- eduisland/src/pages/matiere/Matiere.jsx
+++ eduisland/src/pages/matiere/Matiere.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { supabase } from "../../services/supabaseClient";
 import NiveauCard from "../../components/NiveauCard";
-import { useNavigate } from "react-router-dom";
+
 export default function Matiere() {
   const { id } = useParams();
   const [niveaux, setNiveaux] = useState([]);
   const navigate = useNavigate();
+
   useEffect(() => {
     const fetchNiveaux = async () => {
       const { data, error } = await supabase
@@ -19,12 +20,17 @@ export default function Matiere() {
     fetchNiveaux();
   }, [id]);
 
+  const handleSelect = (niv) => {
+    navigate(`/niveau/${niv.id}`);
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-sky-100 via-green-100 to-emerald-200 p-10 font-[Poppins]">
       <h1 className="text-4xl font-bold text-emerald-700 mb-10 drop-shadow-md">
         🎓 Choisis ton niveau
       </h1>
 
+      {/* Liste verticale des niveaux */}
       <div className="flex flex-col items-center gap-10 relative">
         {niveaux.map((niveau, index) => (
           <NiveauCard
@@ -32,10 +38,11 @@ export default function Matiere() {
             niveau={niveau}
             index={index}
             total={niveaux.length}
-            onClick={(niv) => console.log("Go to level", niv)}
+            onClick={handleSelect}
           />
         ))}
       </div>
+
       <button
         onClick={() => navigate("/")}
         className="mt-12 z-10 bg-emerald-600 text-white px-6 py-3 rounded-xl shadow-md hover:bg-emerald-700 transition-all"
